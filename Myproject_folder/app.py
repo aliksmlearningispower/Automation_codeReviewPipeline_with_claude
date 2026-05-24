1a7ac710fdc2bdd7c77816015ae47a08730589fd
aliksmlearningispower <aliksm.learningispower@gmail.com>
2026-05-24 13:23:06 +0530

    chore: For testing purpose I have added fastapi code

---
+++ Myproject_folder/app.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+# Create the FastAPI instance
+app = FastAPI()
+
+# Root endpoint
+@app.get("/")
+async def read_root():
+    return {"message": "Gnani nerchukuntu jeevinch apai gnaname nennu poshinchu, Rakshinchu, meruguparuchu, balaparuchu and lot many more"}
+
+# Endpoint with a path parameter
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "query_param": q}
