04d96875320287fa14bea05baaea8feca9716e17
aliksmlearningispower <aliksm.learningispower@gmail.com>
2026-05-24 13:41:45 +0530

    Added AI code review related files yml file and as well created secret variables in github repo in settings_secret_variables_actions_add_new

---
+++ Myproject_folder/review_code.py
@@ -0,0 +1,46 @@
+import os
+import subprocess
+from groq import Groq
+
+client = Groq(
+    api_key=os.environ["GROQ_API_KEY"]
+)
+
+diff = subprocess.check_output(
+    ["git", "diff", "origin/main...HEAD"]
+).decode()
+
+prompt = f"""
+You are a senior code reviewer.
+
+Review these code changes.
+
+Check:
+- Bugs
+- Security Issues
+- Performance Issues
+- Best Practices
+
+Provide concise feedback.
+
+Code:
+
+{diff}
+"""
+
+response = client.chat.completions.create(
+    model="llama-3.3-70b-versatile",
+    messages=[
+        {
+            "role": "user",
+            "content": prompt
+        }
+    ]
+)
+
+review = response.choices[0].message.content
+
+print(review)
+
+with open("review.txt", "w") as f:
+    f.write(review)
