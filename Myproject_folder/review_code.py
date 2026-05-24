b0a45018184141e210fc5c84577da0330385bd3a
aliksmlearningispower <aliksm.learningispower@gmail.com>
2026-05-24 17:06:11 +0530

    chore: did some changes in the review code

--- Myproject_folder/review_code.py
+++ Myproject_folder/review_code.py
@@ -75,15 +75,7 @@ print(review)
 with open("review.txt", "w") as f:
     f.write(review)
 
-critical_keywords = [
-    # "asdfghjksdfghdsfghjdfghjfg"
-    "Block Merge Immediately"
-    # "syntax error",
-    # "hardcoded password",
-    # "sql injection"
-    # Above things have to be figured out by the llm.. we cannot write manually as like above, it will cause issues
-    # Example: If we have syntax error, then we have to block merge pull request.. but if we write manually, then even if the result is "No syntax error", as syntax error word is present it will block merging
-]
+critical_keywords = ["Block Merge Immediately"]
 
 for keyword in critical_keywords:
     if keyword.lower() in review.lower():
